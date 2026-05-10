90cefd1a486f3035c70fe8d1a2ef75bc917a47df
Vinaysangwan <pawansangwan541@gmail.com>
2026-05-10 15:45:37 +0530

    Texture Redering

--- NEXUS_EDITOR/src/main.cpp
+++ NEXUS_EDITOR/src/main.cpp
@@ -4,6 +4,37 @@
 #include <Windowing/Window.h>
 #include <iostream>
 
+#define STB_IMAGE_IMPLEMENTATION
+#include <stb_image.h>
+
+GLuint LoadTexture(const std::string& filePath, GLint wrap = GL_CLAMP_TO_EDGE, GLint filter = GL_NEAREST)
+{
+  std::string texturePath = std::string(ASSET_PATH) + "textures/" + filePath;
+  
+  GLuint textureID;
+  glGenTextures(1, &textureID);
+  glBindTexture(GL_TEXTURE_2D, textureID);
+
+  int width, height, nChannels;
+  unsigned char *data = stbi_load(texturePath.c_str(), &width, &height, &nChannels, 4);
+  if(!data)
+  {
+    std::cout<<"Failed to Open the texture: "<<texturePath<<std::endl;
+  }
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, wrap);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, wrap);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, filter);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, filter);
+
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
+
+  stbi_image_free(data);
+  glBindTexture(GL_TEXTURE_2D, 0);
+  
+  return textureID;
+}
+
 int main()
 {
   // init SDL
@@ -70,18 +101,124 @@ int main()
   std::cout << "Version:  " << glGetString(GL_VERSION) << '\n';
   std::cout << "GLSL:     " << glGetString(GL_SHADING_LANGUAGE_VERSION) << '\n';
 
+  const char *vertCode = 
+  "#version 440 core\n"
+  "layout(location = 0) in vec3 inPos;\n"
+  "layout(location = 1) in vec2 inTexCoords;\n"
+
+  "layout(location = 0) out vec2 outTexCoords;\n"
+
+  "void main(){\n"
+  " gl_Position = vec4(inPos, 1.0f);\n"
+  " outTexCoords = inTexCoords;\n"
+  "}";
+
+  const char *fragCode = 
+  "#version 440 core\n"
+  "layout(location = 0) in vec2 inTexCoords;\n"
+  "layout(location = 0) out vec4 outColor;\n"
+  "uniform sampler2D uTextureSampler;\n"
+  "void main(){\n"
+  " vec4 textureColor = texture(uTextureSampler, inTexCoords);\n"
+  " outColor = textureColor;\n"
+  "}";
+
+  GLuint vertID = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertID, 1, &vertCode, nullptr);
+  glCompileShader(vertID);
+  int success;
+  glGetShaderiv(vertID, GL_COMPILE_STATUS, &success);
+  if (success == GL_FALSE)
+  {
+    char infoLog[1024] = {0};
+    glGetShaderInfoLog(vertID, 1024, nullptr, infoLog);
+    std::cout<<"Failed to Compile VertexShader: "<<infoLog<<std::endl;
+    return -1;
+  }
+
+  GLuint fragID = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragID, 1, &fragCode, nullptr);
+  glCompileShader(fragID);
+  glGetShaderiv(fragID, GL_COMPILE_STATUS, &success);
+  if (success == GL_FALSE)
+  {
+    char infoLog[1024] = {0};
+    glGetShaderInfoLog(fragID, 1024, nullptr, infoLog);
+    std::cout<<"Failed to Compile FragmentShader: "<<infoLog<<std::endl;
+    return -1;
+  }
+
+  GLuint programID = glCreateProgram();
+  glAttachShader(programID, vertID);
+  glAttachShader(programID, fragID);
+  glLinkProgram(programID);
+  glGetProgramiv(programID, GL_LINK_STATUS, &success);
+  if (success == GL_FALSE)
+  {
+    char infoLog[1024] = {0};
+    glGetProgramInfoLog(programID, 1024, nullptr, infoLog);
+    std::cout<<"Failed to Link Program: "<<infoLog<<std::endl;
+    return -1;
+  }
+  glDetachShader(programID, vertID);
+  glDetachShader(programID, fragID);
+  glDeleteShader(vertID);
+  glDeleteShader(fragID);
+
   // vertices
   float vertices[] = {
     -0.5f,  0.5f,  0.0f,
     -0.5f, -0.5f,  0.0f,
+     0.5f, -0.5f,  0.0f,
      0.5f,  0.5f,  0.0f,
-
-     0.5f,  0.5f,  0.0f,
-    -0.5f, -0.5f,  0.0f,
-     0.5f, -0.5f,  0.0f
   };
 
+  // texture coords
+  float texCoords[] = {
+    0.0f, 0.0f,
+    0.0f, 1.0f,
+    1.0f, 1.0f,
+    1.0f, 0.0f,
+  };
+  
+  // indices
+  unsigned int indices[] = {
+    0, 1, 3,
+    3, 1, 2
+  };
 
+  // vao
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // ebo
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+  // vbo
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  glEnableVertexAttribArray(0);
+
+  GLuint tex_vbo;
+  glGenBuffers(1, &tex_vbo);
+  glBindBuffer(GL_ARRAY_BUFFER, tex_vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(texCoords), texCoords, GL_STATIC_DRAW);
+
+  glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 0, (void*)0);
+  glEnableVertexAttribArray(1);
+
+  // texture id
+  GLuint textureID = LoadTexture("download.jpg");
+
+  glBindVertexArray(0);
 
   // game loop
   bool running = true;
@@ -109,15 +246,30 @@ int main()
     }
 
     glViewport(
-      window.GetXPos(), window.GetYPos(),
+      0, 0,
       window.GetWidth(), window.GetHeight()
     );
 
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT);
 
+    glUseProgram(programID);
+    glBindVertexArray(vao);
+
+    glActiveTexture(GL_TEXTURE0);
+    glBindTexture(GL_TEXTURE_2D, textureID);
+
+    glDrawElements(GL_TRIANGLES, sizeof(indices) / sizeof(indices[0]), GL_UNSIGNED_INT, 0);
+    
+    glBindVertexArray(0);
+    glUseProgram(0);
+
     SDL_GL_SwapWindow(window.GetWindow().get());
   }
+
+  glDeleteVertexArrays(1, &vao);
+  glDeleteBuffers(1, &ebo);
+  glDeleteProgram(programID);
 
   std::cout<<"Closing"<<std::endl;
 }
